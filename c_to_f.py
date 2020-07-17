11670af58817153fc27192b81715ce7011cf97c5
Aaronnnnn <aaron@MacBook-Pro.local>
2020-07-17 16:44:05 +0800

    add new file

---
+++ c_to_f.py
@@ -0,0 +1,4 @@
+celsius_temperture = input ('請輸入攝氏溫度:')
+celsius_temperture = float(celsius_temperture)
+fahrenheit_temperture = celsius_temperture * 9 / 5 + 32
+print ('華氏溫度:', fahrenheit_temperture)
